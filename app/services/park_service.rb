d9467c1ff90836fa0084d72775b152257e8cdd20
Emily Port <emilylport@gmail.com>
2022-12-05 09:44:52 -0700

    refactor park service

--- app/services/park_service.rb
+++ app/services/park_service.rb
@@ -1,10 +1,12 @@
 class ParkService 
-  def self.search(state)
-    conn = Faraday.new("https://developer.nps.gov/api/v1/") do |faraday|
+
+  def self.conn 
+    Faraday.new("https://developer.nps.gov/api/v1/") do |faraday|
       faraday.params["api_key"] = ENV["parks_api"]
       faraday.adapter Faraday.default_adapter
     end
-
+  end
+  def self.search(state)
     response = conn.get("parks?stateCode=#{state}")
     json = JSON.parse(response.body, symbolize_names: true)
   end
